fdbd6e4a5819976dd6a5d42f203f384a4d3cce22
Ivan <uvanes21@yandex.ru>
2022-07-17 20:44:23 +0300

    Initial commit

---
+++ src/main/java/tgbots/secondbot/SecondBotApplication.java
@@ -0,0 +1,13 @@
+package tgbots.secondbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgbots/secondbot/SecondBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tgbots.secondbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
